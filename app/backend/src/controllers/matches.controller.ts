391d6b0fe9890e12f2807f2c67266bf217937175
Elros <elros@pop-os.localdomain>
2022-12-06 22:22:22 -0300

    i solved requirement 22 and 23

--- app/backend/src/controllers/matches.controller.ts
+++ app/backend/src/controllers/matches.controller.ts
@@ -2,9 +2,17 @@ import { Request, Response } from 'express';
 import MatchesService from '../services/matches.service';
 
 export default class MatchesController {
+  private matchesService = new MatchesService();
   public getAllMatches = async (req: Request, res: Response) => {
-    const matchesService = new MatchesService();
-    const { status, message } = await matchesService.getAllMatches();
-    return res.status(status).json(message);
+    const { inProgress } = req.query;
+    if (!inProgress) {
+      const { status, message } = await this.matchesService.getAllMatches();
+      return res.status(status).json(message);
+    }
+    if (inProgress) {
+      const { status, message } = await this.matchesService
+        .getAllMatchesByProgress(inProgress as string);
+      if (status && message) return res.status(status).json(message);
+    }
   };
 }
--- app/backend/src/services/matches.service.ts
+++ app/backend/src/services/matches.service.ts
@@ -2,12 +2,25 @@ import MatchModel from '../database/models/Match.model';
 import TeamModel from '../database/models/Team.model';
 import statusHTTP from '../utils/statusHTTP';
 
+const INCLUDE = [
+  { model: TeamModel, as: 'teamHome', attributes: ['teamName'] },
+  { model: TeamModel, as: 'teamAway', attributes: ['teamName'] },
+];
+
 export default class MatchesService {
   public getAllMatches = async () => {
-    const allMatches = await MatchModel.findAll({ include: [
-      { model: TeamModel, as: 'teamHome', attributes: ['teamName'] },
-      { model: TeamModel, as: 'teamAway', attributes: ['teamName'] },
-    ] });
+    const allMatches = await MatchModel.findAll({ include: INCLUDE });
     return { status: statusHTTP.OK, message: allMatches };
+  };
+
+  public getAllMatchesByProgress = async (inProgress: string) => {
+    if (inProgress === 'true') {
+      const matchesInProgress = await MatchModel
+        .findAll({ include: INCLUDE, where: { inProgress: 1 } });
+      return { status: statusHTTP.OK, message: matchesInProgress };
+    }
+    const matchesNotInProgress = await MatchModel
+      .findAll({ include: INCLUDE, where: { inProgress: 0 } });
+    return { status: statusHTTP.OK, message: matchesNotInProgress };
   };
 }
